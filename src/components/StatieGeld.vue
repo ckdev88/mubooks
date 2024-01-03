b8020d2c6653a4891b5f18908343392074fd669b
ckdev88 <ckdev88@outlook.com>
2024-01-03 11:31:34 -0300

    fix bla

--- src/components/StatieGeld.vue
+++ src/components/StatieGeld.vue
@@ -3,9 +3,13 @@ import { useStatiegeldStore } from '../stores/StatiegeldStore'
 const StatiegeldStore = useStatiegeldStore()
 </script>
 <template>
-  <h1>Statiegeld bier</h1>
-  <form @submit.prevent="StatiegeldStore.statiegeldBonKratjes(StatiegeldStore.aantalKratjes)">
-    <label>Aantal kratjes <input type="number" v-model="StatiegeldStore.aantalKratjes" /></label>
-    <div>Prijs per krat: {{ StatiegeldStore.statiegeldKrat }}</div>
-    <input type="submit" />
-  </form>
+	<h1>Statiegeld bier</h1>
+	<form @submit.prevent="StatiegeldStore.statiegeldBonKratjes(StatiegeldStore.aantalKratjes)">
+		<label
+			>Aantal kratjes <input type="number" v-model="StatiegeldStore.aantalKratjes"
+		/></label>
+		<div>Prijs per krat: {{ StatiegeldStore.statiegeldKrat }}</div>
+		<input type="submit" />
+	</form>
+	<h3>Statiegeld: {{ StatiegeldStore.statiegeldBon }}</h3>
+</template>
