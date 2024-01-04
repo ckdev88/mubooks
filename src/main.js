b8cce34ffcace841ed8726cf2a9c5041b39b0c6b
ckdev88 <ckdev88@outlook.com>
2024-01-04 06:36:55 -0300

    add simple light theme

--- src/main.js
+++ src/main.js
@@ -9,11 +9,17 @@ import App from './App.vue'
 
 const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+	history: createWebHistory(),
+	routes
 })
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
 app.mount('#app')
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: adark)').matches) {
+	document.getElementsByTagName('html')[0].classList.add('dark-mode')
+} else {
+	document.getElementsByTagName('html')[0].classList.add('light-mode')
+}
