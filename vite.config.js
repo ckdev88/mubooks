5ee718e5d85927c20485386c1200ee3ec3939328
ckdev88 <ckdev88@outlook.com>
2024-01-19 19:09:27 -0300

    testing with oldie

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-	// base: '/mubook/', // can just use vite build --base=/my/public/path/., dus yarn build
-	// --base=/mubook/ voor gh
 	plugins: [vue()],
 	resolve: {
 		alias: {
