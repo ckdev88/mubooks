d06bc08cc76e5542d20868af1ba086a26bae6dc0
ckdev88 <ckdev88@outlook.com>
2024-01-19 21:15:22 -0300

    testing with oldie hacky authstore

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import './assets/main.css'
 
 import { createApp, ref } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 import { routes } from './router'
 
 import { supabase } from '/src/clients/supabase'
 
 import App from './App.vue'
+// import { useAuthStore } from './stores/AuthStore' // 1337 deze erin
 
 const app = createApp(App)
 let router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes
 })
 
@@ -41,9 +42,40 @@ router.beforeEach(async (to, from) => {
 	document.body.classList.add(to.name)
 	document.body.classList.remove(from.name)
 
+
+
+// 1337 deze erin
+/*
+	// hacky bit ... TODO: make less hacky
+	const authStore = useAuthStore()
+	const localUser = await supabase.auth.getSession()
+	console.log('localuser:', localUser)
+
+	if (localUser.data.session === null) {
+		console.log('not logged in, redirect to login in case not in logout page')
+	} else {
+		authStore.setStatus(
+			true,
+			localUser.data.session.user.user_metadata.screenname,
+			localUser.data.session.email,
+			localUser.data.session.email,
+			localUser.data.session.id
+		)
+	}
+	console.log('localuser email:', localUser.data.session.user.email)
+	console.log('localuser screenname:', localUser.data.session.user.user_metadata.screenname)
+	// /hacky bit
+
+*/
+// /1337 deze erin
+
+
+
+
+
 	if (to.meta.requiresAuth || to.meta.requiresNoAuth) {
 		console.log('to.meta.requiresAuth:', to.meta.requiresAuth)
-		const localUser = await supabase.auth.getSession()
+		const localUser = await supabase.auth.getSession() // 1337 deze weg
 		if (localUser.data.session === null) isAuthenticated = false
 		else isAuthenticated = true
 		if (isAuthenticated.value === false && to.meta.requiresAuth) {
@@ -51,7 +83,7 @@ router.beforeEach(async (to, from) => {
 			return { name: 'login' }
 		} else if (isAuthenticated === true && to.meta.requiresNoAuth) {
 			// already authenticated, redirect to profile
-			return { name: 'profile' }
+			return { name: 'dashboard' }
 		}
 	}
 })
--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,16 +1,79 @@
 import { defineStore } from 'pinia'
-
+import { supabase } from '../clients/supabase'
 // TODO: apply supabase to global states
 
+// TODO: apply supabase to global states
+const localSession = async () => {
+	return await supabase.auth.getSession()
+}
+console.log('localsession:', localSession)
+
+/* 1337 deze erin
+const localSession = await supabase.auth.getSession()
+
+var st
+var sc
+var us
+var em
+var id
+if (localSession.data.session === null) {
+	console.log('go login, there is no supabase session')
+	st = false
+	sc = null
+	us = null
+	em = null
+	id = null
+} else {
+	console.log('logged in, update pinia store with localsession userdata')
+	console.log(localSession.data.session)
+	const localUser = localSession.data.session.user
+	st = true
+	sc = localUser.user_metadata.screenname
+	us = localUser.email
+	em = localUser.email
+	id = localUser.id
+	console.log('localuser in AuthStore.js', localUser)
+}
+*/
+
+
+
+
+
+
+
+
 export const useAuthStore = defineStore('AuthStore', {
+	
+
+/* 1337 deze erin
 	state: () => ({
+		status: st,
+		screenname: sc,
+		username: us,
+		email: em,
+		uid: id,
+		link: 'login'
+		// status: false,
+		// screenname: '',
+		// username: '',
+		// email: '',
+		// uid: '',
+		// link: 'login'
+	}),
+*/
+
+
+
+
+state: () => ({
 		status: false,
 		screenname: '',
 		username: '',
 		email: '',
 		uid: '',
 		link: 'login'
-	}),
+	}), // 1337 deze state eruit
 	actions: {
 		setUsername(s = '') {
 			this.username = s
@@ -34,6 +97,22 @@ export const useAuthStore = defineStore('AuthStore', {
 		setUid(s = '') {
 			// user.id
 			this.uid = s
+		},
+		setStatus(status, screenname, username, email, uid) {
+			// TODO: bit hacky, needs to be better,
+			// see main.js for hacky call
+			this.status = status
+			this.screenname = screenname
+			this.username = username
+			this.email = email
+			uid = uid
+		},
+		doLogout() {
+			this.status = false
+			this.screenname = null
+			this.username = null
+			this.email = null
+			this.uid = null
 		}
 	}
 })
